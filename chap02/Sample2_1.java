af0f28d4d5ffae0e18beb0716c3e80ad0f94ac99
rowena22323 <rowena22323@gmail.com>
2021-03-29 22:10:59 +0900

    リテラル

---
+++ chap02/Sample2_1.java
@@ -0,0 +1,14 @@
+class Sample2_1{
+    public static void main(String[]args){
+        System.out.println(255);
+        System.out.println(0377);
+        System.out.println(0xff);
+        System.out.println(0b101);
+        System.out.println(12.33);
+        System.out.println(3e4);
+        System.out.println('A');
+        System.out.println('\u3012');
+        System.out.println("Hello");
+        System.out.println(true);
+    }
+}
